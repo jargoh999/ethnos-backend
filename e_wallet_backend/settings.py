0f16c7fb8e3cc598708d6a698eb7ebccb3e9337b
jargoh999 <151412083+jargoh999@users.noreply.github.com>
2025-02-12 06:50:43 +0100

    allowed origins updated

--- e_wallet_backend/settings.py
+++ e_wallet_backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", "https://appa-38622.web.app"  
+    "http://localhost:5173", "https://appa-38622.web.app","*"  
 ]
 ROOT_URLCONF = 'e_wallet_backend.urls'
 
